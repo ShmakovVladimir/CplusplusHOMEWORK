807fa2fb2a9bd0e4983096c9533752402f5d1ddf
ShmakovVladimir <kaktusation@gmil.com>
2022-09-11 16:59:12 +0300

    LCS

--- lesson_2/D.cpp
+++ lesson_2/D.cpp
@@ -8,50 +8,45 @@ int* inputArray(int size){
     }
     return arr;
 }
-int translateIndex(int i,int j,int sizeX){
-    return i*sizeX+j;
+int translateIndex(int i,int j,int sizeOfArr){
+    return i*sizeOfArr+j;
 }
 int* findSubSeqeunce(int* seq1,int* seq2,int seqSize1,int seqSize2){
-    int dynamicArrayLength = seqSize1*seqSize2;
-    int* subLength = new int[dynamicArrayLength];
-    for(int i = 0; i < dynamicArrayLength; i++){
-        subLength[i] = 0;
-    }
-    for(int i = 1;i<seqSize1;i++){
-        int firstSeqEl = seq1[i-1];
-        for(int j = 1;j<seqSize2;j++){
-            int secondSeqEl = seq2[j-1];
-            if(firstSeqEl==secondSeqEl)
-                subLength[translateIndex(i,j,seqSize1)] = subLength[translateIndex(i-1,j-1,seqSize1)]+1;
+    int subSeqLengthDim = (seqSize1+1)*(seqSize2+1);
+    int* subSeqLength = new int[subSeqLengthDim];
+    for(int i = 0; i<subSeqLengthDim;i++)
+        subSeqLength[i] = 0;
+    for(int i = 1; i<seqSize1+1;i++){
+        int firstSeqElement = seq1[i-1];
+        for(int j = 1; j<seqSize2+1;j++){
+            int secondSeqElement = seq2[j-1];
+            if(firstSeqElement == secondSeqElement)
+                subSeqLength[translateIndex(i,j,seqSize2)] = subSeqLength[translateIndex(i-1,j-1,seqSize2)]+1;
             else
-                subLength[translateIndex(i,j,seqSize1)] = std::max(subLength[translateIndex(i-1,j,seqSize1)],
-                                                                      subLength[translateIndex(i,j-1,seqSize1)]);
+                subSeqLength[translateIndex(i,j,seqSize2)] = std::max(subSeqLength[translateIndex(i-1,j,seqSize2)],subSeqLength[translateIndex(i,j-1,seqSize2)]);
         }
     }
-    bool flag = (seq1[seqSize1-1] == seq2[seqSize2-1]);
-    int maxSubLength = subLength[dynamicArrayLength-1]+(int)flag+1;
-    int* subSeq = new int[maxSubLength];
-    int indexCounter = (int)flag+1;
-    subSeq[0] = maxSubLength;
-    if(flag)
-        subSeq[1] = seq1[seqSize1-1];
     int i = seqSize1;
     int j = seqSize2;
-    while(i && j){
-        int firstSeqEl = seq1[i-1];
-        int secondSeqEl = seq2[j-1];
-        if(firstSeqEl == secondSeqEl){
-            subSeq[indexCounter] = firstSeqEl;
+    int maxSubSequenceLength = subSeqLength[translateIndex(i,j,seqSize2)]+1; //в первом элементе массива будем хранить его длину
+    int* subSeq = new int[maxSubSequenceLength];
+    subSeq[0] = maxSubSequenceLength;
+    int indexCounter = 1;
+    while(i && j && maxSubSequenceLength-indexCounter){
+        int firstSeqElement = seq1[i-1];
+        int secondSeqElement = seq2[j-1];
+        if(firstSeqElement==secondSeqElement){
+            subSeq[indexCounter] = firstSeqElement;
             indexCounter++;
             i--;
             j--;
         }
+        else if(subSeqLength[translateIndex(i-1,j,seqSize2)]>=subSeqLength[translateIndex(i,j-1,seqSize2)])
+            i--;
         else
-            i-=(int)(subLength[translateIndex(i-1,j,seqSize1)]>=subLength[translateIndex(i,j-1,seqSize1)]);
-            j-=(int)(subLength[translateIndex(i-1,j,seqSize1)]<subLength[translateIndex(i,j-1,seqSize1)]);
-        
+            j--;   
     }
-    delete[] subLength;
+    delete[] subSeqLength;
     return subSeq;
 }
 
@@ -76,7 +71,7 @@ int main(){
     std::cout<<std::endl;
     arrayOut(seq2,seqSize2);
     int* maxSubSeq = findSubSeqeunce(seq1,seq2,seqSize1,seqSize2);
-    std::cout<<std::endl<<maxSubSeq[0];
+    std::cout<<std::endl<<"Greatest sub sequence length is: "<<maxSubSeq[0]-1;
     std::cout<<std::endl<<"Greatest sub sequence is: ";
     for(int i = maxSubSeq[0]-1;i>0;i--){
         std::cout<<maxSubSeq[i]<<" ";
