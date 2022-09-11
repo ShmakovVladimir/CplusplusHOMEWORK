26ba2f2fdc537fab10df25e3791993cd42195cfa
ShmakovVladimir <kaktusation@gmil.com>
2022-09-11 20:25:02 +0300

    Наконец осознал ошибку. Размерность массива (f1+1)*(f2+1). А в функцию translate я предавал просто f2.

--- lesson_2/D.cpp
+++ lesson_2/D.cpp
@@ -1,5 +1,58 @@
 #include <iostream>
-#include <algorithm>
+
+
+int translate(int i,int j,int numberOfCollumns){
+    return i*numberOfCollumns+j;
+}
+
+int* LCS(int* sequnce1,int* sequence2,int firstSequenceLength,int secondSequenceLength){
+    int sliceLengthArrDim = (firstSequenceLength+1)*(secondSequenceLength+1);
+    int* sliceLength  = new int[sliceLengthArrDim];
+    for(int i = 0;i<sliceLengthArrDim;i++){
+        sliceLength[i] = 0;
+    }
+
+    for(int i = 1;i<=firstSequenceLength;i++){
+        int firstSeqElement = sequnce1[i-1];
+        for(int j = 1;j<=secondSequenceLength;j++){
+            int secondSeqElement = sequence2[j-1];
+            int left = sliceLength[translate(i,j-1,secondSequenceLength+1)];
+            int up = sliceLength[translate(i-1,j,secondSequenceLength+1)];
+            int diagonal = sliceLength[translate(i-1,j-1,secondSequenceLength+1)];
+            if(secondSeqElement==firstSeqElement)
+                sliceLength[translate(i,j,secondSequenceLength+1)] = diagonal+1;
+            else
+                sliceLength[translate(i,j,secondSequenceLength+1)] = std::max(left,up);
+        }
+    }
+    int lcsLength = sliceLength[sliceLengthArrDim-1]+1; //по нулевому индексу храним длину НОП
+    int* lcs = new int[lcsLength];
+    lcs[0] = lcsLength;
+    int i = firstSequenceLength;
+    int j = secondSequenceLength;
+    int indexCounter = 1;
+    while(lcsLength>indexCounter&&i&&j){
+        int left = sliceLength[translate(i,j-1,secondSequenceLength+1)];
+        int up = sliceLength[translate(i-1,j,secondSequenceLength+1)];
+        if(sequnce1[i-1]==sequence2[j-1]){
+            lcs[indexCounter] = sequnce1[i-1];
+            i--;
+            j--;
+            indexCounter++;
+        }
+        else if(left>up)
+            j--;
+        else
+            i--;  
+    }
+    delete[] sliceLength; 
+    return lcs;
+}
+void arrayOut(int* arr,int size){
+    for(int i = 0;i<size;i++){
+        std::cout<<arr[i]<<" ";
+    }
+}
 int* inputArray(int size){
     int* arr = new int[size];
     for(int i = 0;i<size;i++){
@@ -8,59 +61,11 @@ int* inputArray(int size){
     }
     return arr;
 }
-int translateIndex(int i,int j,int sizeOfArr){
-    return i*sizeOfArr+j;
-}
-int* findSubSeqeunce(int* seq1,int* seq2,int seqSize1,int seqSize2){
-    int subSeqLengthDim = (seqSize1+1)*(seqSize2+1);
-    int* subSeqLength = new int[subSeqLengthDim];
-    for(int i = 0; i<subSeqLengthDim;i++)
-        subSeqLength[i] = 0;
-    for(int i = 1; i<seqSize1+1;i++){
-        int firstSeqElement = seq1[i-1];
-        for(int j = 1; j<seqSize2+1;j++){
-            int secondSeqElement = seq2[j-1];
-            if(firstSeqElement == secondSeqElement)
-                subSeqLength[translateIndex(i,j,seqSize2)] = subSeqLength[translateIndex(i-1,j-1,seqSize2)]+1;
-            else
-                subSeqLength[translateIndex(i,j,seqSize2)] = std::max(subSeqLength[translateIndex(i-1,j,seqSize2)],subSeqLength[translateIndex(i,j-1,seqSize2)]);
-        }
-    }
-    int i = seqSize1;
-    int j = seqSize2;
-    int maxSubSequenceLength = subSeqLength[translateIndex(i,j,seqSize2)]+1; //в первом элементе массива будем хранить его длину
-    int* subSeq = new int[maxSubSequenceLength];
-    subSeq[0] = maxSubSequenceLength;
-    int indexCounter = 1;
-    while(i && j && maxSubSequenceLength-indexCounter){
-        int firstSeqElement = seq1[i-1];
-        int secondSeqElement = seq2[j-1];
-        if(firstSeqElement==secondSeqElement){
-            subSeq[indexCounter] = firstSeqElement;
-            indexCounter++;
-            i--;
-            j--;
-        }
-        else if(subSeqLength[translateIndex(i-1,j,seqSize2)]>=subSeqLength[translateIndex(i,j-1,seqSize2)])
-            i--;
-        else
-            j--;   
-    }
-    delete[] subSeqLength;
-    return subSeq;
-}
-
-void arrayOut(int* arr,int size){
-    for(int i = 0;i<size;i++){
-        std::cout<<arr[i]<<" ";
-    }
-}
-
 int main(){
     int seqSize1,seqSize2;
     std::cout<<"Enter size of the first sequence: ";
     std::cin>>seqSize1;
-    std::cout<<std::endl<<"Enter size of the second sequence: ";
+    std::cout<<"Enter size of the second sequence: ";
     std::cin>>seqSize2;
     std::cout<<std::endl<<"Enter the first sequence: "<<std::endl;
     int *seq1 = inputArray(seqSize1);
@@ -70,7 +75,7 @@ int main(){
     arrayOut(seq1,seqSize1);
     std::cout<<std::endl;
     arrayOut(seq2,seqSize2);
-    int* maxSubSeq = findSubSeqeunce(seq1,seq2,seqSize1,seqSize2);
+    int* maxSubSeq = LCS(seq1,seq2,seqSize1,seqSize2);
     std::cout<<std::endl<<"Greatest sub sequence length is: "<<maxSubSeq[0]-1;
     std::cout<<std::endl<<"Greatest sub sequence is: ";
     for(int i = maxSubSeq[0]-1;i>0;i--){
