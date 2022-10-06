c4520e493a7a1f978ca8dac8018e95017509c469
ShmakovVladimir <kaktusation@gmil.com>
2022-10-07 00:06:37 +0300

    forMd

---
+++ lesson_4/Bend.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+struct Student
+{
+
+
+
+    unsigned int course;
+    std::string studentName;
+};
+
+
+int main()
+{
+
+    return 0;
+}
