21e94bcb47eee6f6e7e1a07b50c1eb39e4d3c5e6
ShmakovVladimir <kaktusation@gmil.com>
2022-10-09 19:59:05 +0300

    task2 changes

--- lesson_4/Bend.cpp
+++ lesson_4/Bend.cpp
@@ -6,16 +6,19 @@ struct Lesson;
 struct Student;
 struct Student
 {
-    Student()
+    Student(): name {"None"},course {0}
     {
-        this->name = "None";
-        this->course = 0;
+        
     }
-    Student(std::string name,unsigned int course): name {name},course {course}
+    Student(const std::string& studentName,unsigned int course): name {studentName},course {course}
     {
 
     }
-    void addLesson(Lesson* lessonToAdd);
+    void addLesson(Lesson* lessonToAdd)
+    {
+        lessons.push_back(lessonToAdd);
+    }
+    void printInfo() const;
     std::string name;
     unsigned int course;
     std::vector<Lesson*> lessons;
@@ -23,19 +26,17 @@ struct Student
 
 struct Lesson
 {
-    Lesson()
+    Lesson(): lessonName {"None"}, placeName {"None"}, teacherName {"None"}
     {
-        this->lessonName = "None";
-        this->placeName = "None";
-        this->teacherName = "None";
+
     }
-    Lesson(std::string lessonName,std::string teacherName,std::string placeName): lessonName {lessonName},teacherName {teacherName},placeName {placeName}
+    Lesson(const std::string& lesson,const std::string& teacher,const std::string& place): lessonName {lesson},teacherName {teacher},placeName {place}
     {
 
     }
     void addStudent(Student* studentToAdd)
     {
-        this->students.push_back(studentToAdd);
+        students.push_back(studentToAdd);
     }
     void printInfo() const;
     std::string lessonName;
@@ -43,12 +44,9 @@ struct Lesson
     std::string placeName;
     std::vector<Student*> students;
 };
-void Student::addLesson(Lesson* lessonToAdd)
+
+void Lesson::printInfo() const
 {
-    this->lessons.push_back(lessonToAdd);
-    lessonToAdd->addStudent(this);
-}
-void Lesson::printInfo() const{
     std::cout<<std::endl<<"Lesson: "<<this->lessonName<<" Teacher: "<<this->teacherName<<" Place: "<<this->placeName;
     std::cout<<std::endl<<"List of students on this lesson:";
     for(int i = 0;i<this->students.size();i++)
@@ -57,27 +55,38 @@ void Lesson::printInfo() const{
     }
 }
 
+void Student::printInfo() const
+{
+    std::cout<<std::endl<<"Student: "<<name<<" course: "<<course;
+    std::cout<<std::endl<<"List of lessons for this student: ";
+    for(int i = 0; i< lessons.size();i++)
+    {
+        std::cout<<std::endl<<"lesson "<<i+1<<" : "<<lessons[i]->lessonName;
+    }
+}
+
+
+void addLessonForStudent(Student* studentForALesson,Lesson* lessonForAStudent)
+{
+    studentForALesson->addLesson(lessonForAStudent);
+    lessonForAStudent->addStudent(studentForALesson);
+}
+
 int main()
 {
-    std::vector<Student*> b04_105_students;
-    std::vector<Lesson*> lessons;
-    lessons.push_back(new Lesson("differential equation","Umnov E.A.","411 gk"));
-    b04_105_students.push_back(new Student("Saltykova DiU",2));
-    b04_105_students.push_back(new Student("Sifat M.D.",2));
-    b04_105_students.push_back(new Student("Dolya Artemiy",2));
-    b04_105_students.push_back(new Student("Shmakov V",2));
+    std::vector<Student> b04_105_students;
+    std::vector<Lesson> lessons;
+    lessons.push_back(Lesson("differential equation","Umnov E.A.","411 gk"));
+    b04_105_students.push_back(Student("Saltykova DiU",2));
+    b04_105_students.push_back(Student("Sifat M.D.",2));
+    b04_105_students.push_back(Student("Dolya Artemiy",2));
+    b04_105_students.push_back(Student("Shmakov V",2));
     for(int i = 0;i<b04_105_students.size();i++)
     {
-        b04_105_students[i]->addLesson(lessons[0]);
+        addLessonForStudent(&b04_105_students[i],&lessons[0]);
     }
-    lessons[0]->printInfo();
-    for(int i = 0;i<b04_105_students.size();i++)
-    {
-        delete b04_105_students[i];
-    }
-    for(int i = 0;i<lessons.size();i++)
-    {
-        delete lessons[i];
-    }
+    lessons[0].printInfo();
+    std::cout<<"\n \n";
+    b04_105_students[0].printInfo();
     return 0;
 }
