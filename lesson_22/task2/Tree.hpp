028567a16e81eabfc7c2123e93abc3f839d84074
Vladimir <kaktusation@gmail.com>
2023-03-07 21:21:30 +0300

    tree Done

--- lesson_22/task2/Tree.hpp
+++ lesson_22/task2/Tree.hpp
@@ -1,17 +1,56 @@
 #pragma once
-#include <iostream>
 #include <memory>
-#include "Node.hpp"
+#include <iostream>
 
+template <typename T>
+class Tree {
+    class Node {
+        public:
+            std::weak_ptr<Node> parent;
+            std::shared_ptr<Node> child_left;
+            std::shared_ptr<Node> child_right;
+            T value;
 
+            Node(const T& val) : value(val), child_left(nullptr), child_right(nullptr) {}
 
+            ~Node() {
+            std::cout << "Node with value " << value << " is deleted" << std::endl;
+            value.~T();
+            }
+    };
 
+    std::shared_ptr<Node> head;
+    std::shared_ptr<Node> active;
+public:
+    void AppendLeft(const T& val) {
+        active->child_left = std::make_shared<Node>(val); 
+        active->child_left->parent = std::weak_ptr<Node>(active);
+    }
 
-class Tree()
-{
-    public:
-        Tree(key_t rootElement): root(rootElement, nullptr);
-        
-    private:
-        TreeNode<key_t> root;
+    void AppendRight(const T& val) {
+        active->child_right = std::make_shared<Node>(val);
+        active->child_right->parent = std::weak_ptr<Node>(active);
+    }
+    Tree(const T& val) {
+        head = std::make_shared<Node>(val);
+        active = head;
+    }
+    void append(const T& val)
+    {
+        if(val <= (active->value) && !active->child_left)
+            AppendLeft(val);
+        else if(val <= active->value)
+        {
+            active = active->child_left;
+            append(val);
+        }
+         if(val > active->value && !active->child_left)
+            AppendRight(val);
+        else if(val > active->value)
+        {
+            active = active->child_right;
+            append(val);
+        }
+    }
+
 };
